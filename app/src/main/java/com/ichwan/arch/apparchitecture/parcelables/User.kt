c6acc50706491ec27ed327784e65a7011379cf0f
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-10 13:00:04 +0700

    chore: add parcelable implement

---
+++ app/src/main/java/com/ichwan/arch/apparchitecture/parcelables/User.kt
@@ -0,0 +1,40 @@
+package com.ichwan.arch.apparchitecture.parcelables
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class User(
+    val name: String?,
+    val age: Int?,
+    val email: String?,
+    val city: String?
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeValue(age)
+        parcel.writeString(email)
+        parcel.writeString(city)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
